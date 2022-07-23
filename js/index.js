b4b06fa5440129a44d7d3b8c7461febb44468057
zengtx <799895183@qq.com>
2022-07-23 09:50:17 +0800

    修改了样式
    213

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
+
+// 页面加载时显示时间
 let date = document.getElementById('date');
 date.innerHTML = new date();
+
+/*
+function onclick(){
+    let shuRuKuang = document.getElementById('shuRuKuang');
+    if{
+        
+
+    }
+}
+*/
